78fc0239132873cd7dc1e8573413ed3125e2b88f
Nate Santti <31102279+nsantti@users.noreply.github.com>
2018-06-12 10:47:24 -0400

    Add files via upload
    - Players now have a list of pieces they've captured
    - Captured pieces are drawn to the side of the board

--- GameMove.js
+++ GameMove.js
@@ -1,30 +1,32 @@
 class GameMove {
-  constructor(owner, from, to) {
-    this.owner = owner;
-    this.from = from;
-    this.to = to;
-  }
+	constructor(owner, from, to) {
+		this.owner = owner;
+		this.from = from;
+		this.to = to;
+	}
 
-  // Actually doing the move. Mostly transferring instance variables
-  doIt() {
-    this.to.piece = this.from.piece;
-    this.to.owner = this.from.owner;
-    this.to.ownerN = this.from.ownerN;
-    this.to.mustJump = false;
-    if (!this.from.king) {
-      if ((this.to.row == 7 && this.from.ownerN == 1) ||
-        (this.to.row == 0 && this.from.ownerN == 2)) {
-        this.to.king = true;
-      }
-    } else {
-      this.to.king = this.from.king;
-    }
+	// Actually doing the move. Mostly transferring instance variables
+	doIt() {
+		this.to.piece = this.from.piece;
+		this.to.owner = this.from.owner;
+		this.to.ownerN = this.from.ownerN;
+		this.to.mustJump = false;
+		// Handles if the piece should be a king after the jump
+		if (!this.from.king) {
+			if ((this.to.row == 7 && this.from.ownerN == 1) ||
+				(this.to.row == 0 && this.from.ownerN == 2)) {
+				this.to.king = true;
+			}
+		} else {
+			this.to.king = this.from.king;
+		}
 
-    this.from.mustJump = false;
-    this.from.piece = null;
-    this.from.owner = null;
-    this.from.ownerN = 0;
-    this.from.king = false;
-  }
+		// Making the from square empty
+		this.from.mustJump = false;
+		this.from.piece = null;
+		this.from.owner = null;
+		this.from.ownerN = 0;
+		this.from.king = false;
+	}
 
 }
